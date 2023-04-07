2ca9010249cd76a7979b8d7de1c1a57a149cfd25
Gabriel Dalanyi <50451712+gabrieldalanyi@users.noreply.github.com>
2023-04-08 00:03:34 +0300

    Add files via upload

---
+++ src/utils/Constans.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Constans {
+
+        public static final String USER_EMAIl = "dalanyigabriel@gmail.com";
+        public static final String USER_PASS = "#123456789#";
+        public static final String NEW_USER_PASS = "#987654321#";
+        public static final String NEW_CONFIRM_USER_PASS = "#987654321#";
+    }
+
+
